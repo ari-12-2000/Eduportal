fadd4e7de420c3f25e50390897c7b5439ccb7db0
ari-12-2000 <arikum12000@gmail.com>
2025-06-22 21:59:40 +0530

    updated debug-env route

--- app/debug-env/route.ts
+++ app/debug-env/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({
-    dbUrl: process.env.DATABASE_URL || 'Not Found'
+    dbUrl: process.env.DATABASE_URL || 'Not Found',
+    db_drUrl:process.env.DIRECT_URL || 'Not Found'
   });
 }
